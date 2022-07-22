93106daf18e9760efcdf1f73bbf7fd518322458b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-21 22:34:33 -0300

    Update rescale method and fix errors

--- cloud_shadow_mask.py
+++ cloud_shadow_mask.py
@@ -20,53 +20,73 @@ FC = ee.FeatureCollection(FC_PATH)
 
 
 def rescale(img, exp, thresholds):
-    return (img.expression(exp, {img: img}).subtract(thresholds[0]).divide(thresholds[1] - thresholds[0]))
-  
+    return (
+        img.expression(exp, {"img": img})
+        .subtract(thresholds[0])
+        .divide(thresholds[1] - thresholds[0])
+    )
+
 
 def add_cloud_score(img):
-    ndmi = img.normalizedDifference(['nir', 'swir1'])
-    ndsi = img.normalizedDifference(['green', 'swir1'])
+    ndmi = img.normalizedDifference(["nir", "swir1"])
+    ndsi = img.normalizedDifference(["green", "swir1"])
 
     score = ee.Image(1)
-    score = score.min(rescale(img, 'img.blue', [0.1, 0.5]))
-    score = score.min(rescale(img, 'img.red + img.green + img.blue', [0.2, 0.8]))
-    score = score.min(rescale(ndmi, 'img', [-0.1, 0.1]))
-    score = score.min(rescale(ndsi, 'img', [0.4, 0.1]))
+    score = score.min(rescale(img, "img.blue", [0.1, 0.5]))
+    score = score.min(rescale(img, "img.red + img.green + img.blue", [0.2, 0.8]))
+    score = score.min(rescale(ndmi, "img", [-0.1, 0.1]))
+    score = score.min(rescale(ndsi, "img", [0.4, 0.1]))
     score = score.multiply(100).byte()
-    
-    return img.addBands(score.rename('cloudScore'))
 
-def project_shadows(image, cloud_mask, cloud_heights, contract_pixels, dilate_pixels, infrared_threshold):
+    return img.addBands(score.rename("cloudScore"))
 
+
+def project_shadows(
+    image, cloud_mask, cloud_heights, contract_pixels, dilate_pixels, infrared_threshold
+):
     def find_shadow(img):
         # TODO
         return img
 
     right_angle = ee.Number(90)
-    sun_elevation = image.get('SUN_ELEVATION')
-    mean_azimuth = image.get('SUN_AZIMUTH')
+    sun_elevation = image.get("SUN_ELEVATION")
+    mean_azimuth = image.get("SUN_AZIMUTH")
     mean_zenith = right_angle.subtract(sun_elevation)
 
     # Find dark pixels in the image
-    dark_pixels = image.select(['nir', 'swir1', 'swir2']).reduce(ee.Reducer.sum()).lt(infrared_threshold)
-    
+    dark_pixels = (
+        image.select(["nir", "swir1", "swir2"])
+        .reduce(ee.Reducer.sum())
+        .lt(infrared_threshold)
+    )
+
     # Get Scale of image
     nominal_scale = cloud_mask.projection().nominalScale()
 
     # Find where cloud shadows should be based on Solar Geometry and convert it to radians
-    azimuth_radians = ee.Number(mean_azimuth).multiply(math.pi).divide(180.0).add(ee.Number(0.5).multiply(math.pi))
-    zenith_radians  = ee.Number(0.5).multiply(math.pi).subtract(ee.Number(mean_zenith).multiply(math.pi).divide(180.0))
-  
+    azimuth_radians = (
+        ee.Number(mean_azimuth)
+        .multiply(math.pi)
+        .divide(180.0)
+        .add(ee.Number(0.5).multiply(math.pi))
+    )
+    zenith_radians = (
+        ee.Number(0.5)
+        .multiply(math.pi)
+        .subtract(ee.Number(mean_zenith).multiply(math.pi).divide(180.0))
+    )
+
     # Find the shadows
     shadows = cloud_heights.map(find_shadow)
     shadow_mask = ee.ImageCollection.fromImages(shadows).max()
-    shadow_mask = shadow_mask.and(cloud_mask.not())
-    shadow_mask = shadow_mask.focal_min(contract_pixels).focal_max(dilate_pixels)
-    shadow_mask = shadow_mask.and(dark_pixels)
+    # shadow_mask = shadow_mask.and(cloud_mask.not())
+    # shadow_mask = shadow_mask.focal_min(contract_pixels).focal_max(dilate_pixels)
+    # shadow_mask = shadow_mask.and(dark_pixels)
 
-    image = image.addBands(shadow_mask.rename(['cloudShadow']))
+    image = image.addBands(shadow_mask.rename(["cloudShadow"]))
 
     return image
+
 
 def add_cloud_shadow(image):
     ing_id = image.get("ING_ID")
@@ -79,17 +99,27 @@ def add_cloud_shadow(image):
 
     # Get cloud score
     image = add_cloud_score(image)
-    cloud_score = image.select('cloudScore')
+    cloud_score = image.select("cloudScore")
 
     # Mask the cloud score
-    cloud_mask = cloud_score.gt(cloud_threshold).focal_min(contract_pixels).focal_max(dilate_pixels)
+    cloud_mask = (
+        cloud_score.gt(cloud_threshold)
+        .focal_min(contract_pixels)
+        .focal_max(dilate_pixels)
+    )
     # CloudMasked  = image.updateMask(cloud_mask.not()) # TODO check this.... is this needed?
 
     # Projecting the cloud shadow
-    cloud_shadow_masked = project_shadows(image, cloud_mask, cloud_heights, contract_pixels, dilate_pixels, infrared_threshold)
+    cloud_shadow_masked = project_shadows(
+        image,
+        cloud_mask,
+        cloud_heights,
+        contract_pixels,
+        dilate_pixels,
+        infrared_threshold,
+    )
     return cloud_shadow_masked.clip(geometry)
 
 
 if __name__ == "__main__":
     pass
-    
