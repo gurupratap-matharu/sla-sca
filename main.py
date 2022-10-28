31619eb61d998a073fca64bab6f0d5377a774836
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-28 15:41:22 -0300

    Implement analysing a list of ING_IDS

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def write_to_local(response, filename):
     logger.info(f"Results written to {filename}")
 
 
-ing_id = "G718255O411666S"
+ING_IDS = ("G718255O411666S",)
 start_year = ee.Number(2022)
 end_year = ee.Number(2022)
 doy_start = ee.Number(50)
@@ -43,39 +43,45 @@ doy_end = ee.Number(200)
 cloudiness = ee.Number(50)
 coverage = 50
 hsboolean = 0
+hs_start = ee.Number(50)
+hs_end = ee.Number(200)
 dem = "SRTM"
 
+for ing_id in ING_IDS:
+    logger.info("analysing id: %s", ing_id)
 
-preprocessor = PreProcessor(
-    ing_id=ing_id,
-    start_year=start_year,
-    end_year=end_year,
-    doy_start=doy_start,
-    doy_end=doy_end,
-    cloudiness=cloudiness,
-    coverage=coverage,
-    hsboolean=hsboolean,
-    dem=dem,
-)
+    preprocessor = PreProcessor(
+        ing_id=ing_id,
+        start_year=start_year,
+        end_year=end_year,
+        doy_start=doy_start,
+        doy_end=doy_end,
+        cloudiness=cloudiness,
+        coverage=coverage,
+        hsboolean=hsboolean,
+        dem=dem,
+    )
 
-logger.info("preprocessing...")
-preprocessed = preprocessor.execute()
+    logger.info("preprocessing...")
+    preprocessed = preprocessor.execute()
 
-logger.info("processing cloud shadow mask...")
-preprocessed = preprocessed.map(add_cloud_shadow)
+    logger.info("processing cloud shadow mask...")
+    preprocessed = preprocessed.map(add_cloud_shadow)
 
-logger.info("processing hill shadow...")
-preprocessed = add_hill_shadow(image_collection=preprocessed)
+    logger.info("processing hill shadow...")
+    preprocessed = add_hill_shadow(
+        image_collection=preprocessed, hs_start=hs_start, hs_end=hs_end
+    )
 
+    logger.info("initiating classifier...")
+    map_collection = preprocessed.map(decision_tree)
 
-logger.info("initiating classifier...")
-map_collection = preprocessed.map(decision_tree)
+    logger.info("initiating SLA extraction...")
+    map_collection = map_collection.map(extract_sla_patch)
 
-logger.info("processing main patches...")
-map_collection = map_collection.map(extract_sla_patch)
+    # logger.info("writing to local...")
+    # write_to_local(response=map_collection, filename="dump/final.json")
 
-# logger.info("writing to local...")
-# write_to_local(response=map_collection, filename="dump/final.json")
 
 end = time.time()
 logger.info("All Done...!")
